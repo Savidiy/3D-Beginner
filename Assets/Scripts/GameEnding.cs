3c7b65164bec8bba0adcf518eb3603dd3f17cd4f
Savidiy <savidiy@mail.ru>
2021-01-23 12:41:31 +0300

    Game Ending added

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    [SerializeField] float fadeDuration = 1f;
+    [SerializeField] float displayImageDuration = 2f;
+    [SerializeField] GameObject player;
+    [SerializeField] CanvasGroup exitBackgroundImageCanvasGroup;
+    bool _isPlayerAtExit = false;
+    float _timer = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            _isPlayerAtExit = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (_isPlayerAtExit)
+        {
+            EndLevel();
+        }
+    }
+
+    void EndLevel()
+    {
+        _timer += Time.deltaTime;
+
+        exitBackgroundImageCanvasGroup.alpha = _timer / fadeDuration;
+
+        if(_timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
+    }
+}
