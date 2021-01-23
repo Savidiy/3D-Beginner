63790f34fb06f55f8f8ea5feb0c2026ace99e988
Savidiy <savidiy@mail.ru>
2021-01-23 23:21:05 +0300

    add gameover and observer script

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
@@ -8,7 +9,9 @@ public class GameEnding : MonoBehaviour
     [SerializeField] float displayImageDuration = 2f;
     [SerializeField] GameObject player;
     [SerializeField] CanvasGroup exitBackgroundImageCanvasGroup;
+    [SerializeField] CanvasGroup catchBackgroundImageCanvasGroup;
     bool _isPlayerAtExit = false;
+    bool _isPlayerCaught = false;
     float _timer = 0;
 
     private void OnTriggerEnter(Collider other)
@@ -19,23 +22,39 @@ public class GameEnding : MonoBehaviour
         }
     }
 
+    public void CaughtPlayer()
+    {
+        _isPlayerCaught = true;
+    }
+
     private void Update()
     {
         if (_isPlayerAtExit)
         {
-            EndLevel();
+            EndLevel(exitBackgroundImageCanvasGroup, false);
+        } 
+        else if (_isPlayerCaught)
+        {
+            EndLevel(catchBackgroundImageCanvasGroup, true);
         }
     }
 
-    void EndLevel()
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
     {
         _timer += Time.deltaTime;
 
-        exitBackgroundImageCanvasGroup.alpha = _timer / fadeDuration;
+        imageCanvasGroup.alpha = _timer / fadeDuration;
 
         if(_timer > fadeDuration + displayImageDuration)
         {
-            Application.Quit();
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
         }
     }
 }
---
+++ Assets/Scripts/Observer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] GameEnding gameEnding;
+    bool _isPlayerInRange = false;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == player)
+        {
+            _isPlayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform == player)
+        {
+            _isPlayerInRange = false;
+        }
+    }
+
+    Ray ray = new Ray();
+    private void Update()
+    {
+        if(_isPlayerInRange == true)
+        {
+            Vector3 direction = player.position - transform.position + Vector3.up;
+
+            ray = new Ray(transform.position, direction);
+
+            RaycastHit raycastHit;
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                if (raycastHit.collider.transform == player)
+                {
+                    gameEnding.CaughtPlayer();
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_isPlayerInRange == true)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(ray);
+        }
+    }
+}
