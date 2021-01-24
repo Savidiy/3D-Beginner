e6897af045461dac4b5a7398a7eb36177df290c3
Savidiy <savidiy@mail.ru>
2021-01-24 11:52:44 +0300

    add patrol paths

--- Assets/Scripts/WaypointPatrol.cs
+++ Assets/Scripts/WaypointPatrol.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-enum PatrolType {PingPong, Circle, Test}
+enum PatrolType {PingPong, Circle}
 
 
 [RequireComponent(typeof(NavMeshAgent))]
@@ -11,47 +11,39 @@ public class WaypointPatrol : MonoBehaviour
 {
     NavMeshAgent navMeshAgent;
     [SerializeField] GameObject path;
-    Transform[] waypoints;
     [SerializeField] PatrolType patrolType;
     int _currentWaypointIndex;
     bool _isIncreaseWaypointIndex = true;
 
     void Start()
     {
-        if (path != null)
-        {
-            waypoints = new Transform[path.transform.childCount];
-            for (int i = 0; i < path.transform.childCount; i++)
-            {
-                waypoints[i] = path.transform.GetChild(i);
-            }
-        }
-        else
-        {
-            waypoints = new Transform[0];
-        }
         navMeshAgent = GetComponent<NavMeshAgent>();
-        if (waypoints.Length > 0)
-            navMeshAgent.SetDestination(waypoints[0].position);
+        if (path != null && path.transform.childCount > 0)
+        {
+            Vector3 startPoint = path.transform.GetChild(0).position;
+            transform.position = startPoint;
+            navMeshAgent.SetDestination(startPoint);
+        }
     }
 
     void Update()
     {
-        if (waypoints.Length > 0)
+        if (path != null && path.transform.childCount > 0)
         {
             if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
             {
                 switch (patrolType)
                 {
-                    case PatrolType.Circle: /// 01230123
-                        _currentWaypointIndex = (_currentWaypointIndex + 1) % waypoints.Length;
+                    case PatrolType.Circle: /// 0123_0123_0123
+                        _currentWaypointIndex = (_currentWaypointIndex + 1) % path.transform.childCount;
                         break;
-                    case PatrolType.PingPong: /// 0123210123
-                        if (waypoints.Length > 1) {
+                    case PatrolType.PingPong: /// 0123_210_123
+                        if (path.transform.childCount > 1)
+                        {
                             if (_isIncreaseWaypointIndex)
                             {
                                 _currentWaypointIndex++;
-                                if (_currentWaypointIndex >= waypoints.Length)
+                                if (_currentWaypointIndex >= path.transform.childCount)
                                 {
                                     _currentWaypointIndex -= 2;
                                     _isIncreaseWaypointIndex = false;
@@ -68,13 +60,16 @@ public class WaypointPatrol : MonoBehaviour
                             }
                         }
                         break;
-                    default:                        
-                        Debug.LogError($"{this.GetType().Name}: Unknown Patrol Type");                        
+                    default:
+                        Debug.LogError($"{this.GetType().Name}: Unknown Patrol Type");
                         break;
                 }
-                navMeshAgent.SetDestination(waypoints[_currentWaypointIndex].position);
+
+                if (_currentWaypointIndex >= 0 && _currentWaypointIndex < path.transform.childCount)
+                    navMeshAgent.SetDestination(path.transform.GetChild(_currentWaypointIndex).position);
+                else
+                    _currentWaypointIndex = 0;
             }
-        }
-            
+        } 
     }
 }
